558576c2c64f80838338396900b2ad4e4f22af79
AhmedB2023 <abarakati85@gmail.com>
2025-12-25 18:55:16 -0500

    Debugging

--- server.js
+++ server.js
@@ -204,9 +204,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 
-// ✅ Add this line to mount the route
-const idUploadRoutes = require('./routes/idUpload');
-app.use('/api', idUploadRoutes);
+/
 
 
 console.log("🔍 APP_BASE_URL from .env:", process.env.APP_BASE_URL);
